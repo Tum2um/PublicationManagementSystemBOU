1000204a3ac4298fd52e4cafe5de6984e259b7cd
lucynamataaka <lucynamataaka@gmail.com>
2026-07-13 15:30:00 +0300

    Add master data service files

---
+++ services/masterdata/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Department(db.Model):
+    __tablename__ = 'departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    is_active = db.Column(db.Boolean, default=True)
+
+class Theme(db.Model):
+    __tablename__ = 'themes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    is_active = db.Column(db.Boolean, default=True)
+    
